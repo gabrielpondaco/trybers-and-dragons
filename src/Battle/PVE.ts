909c7a6a4b76470fd4852ae3088c0fc4e83f4f07
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-08-09 19:37:15 -0300

    feat: adiciona requisito 11
    cria classe PVE
    simula luta entre um Player(Fighter) e um ou mais Fighters/SimpleFighter

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,22 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  constructor(
+    _player1: Fighter, 
+    private enemy: (Fighter | SimpleFighter)[],
+  ) {
+    super(_player1);
+    this.fight();
+  }
+
+  fight(): number {
+    for (let index = this.enemy.length - 1; index > 0; index -= 1) {
+      while (this.enemy[index].lifePoints > -1 && this.player.lifePoints > -1) {
+        this.player.attack(this.enemy[index]);
+        this.enemy[index].attack(this.player);
+      }
+    }
+    return this.player.lifePoints === -1 ? -1 : 1;
+  }
+}

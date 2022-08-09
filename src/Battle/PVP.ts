a4e08ff13b97644bd675dcbd94b100fa2e42efc9
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-08-09 19:20:12 -0300

    feat: adiciona requisito 10
    cria classe PVP
    e classe abstrata Battle

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,20 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+export default class PVP extends Battle {
+  constructor(
+    private _player2: Fighter,
+    _player1: Fighter, 
+  ) {
+    super(_player1);
+    this.fight();
+  }
+
+  fight(): number {
+    while (this._player2.lifePoints > -1 && this.player.lifePoints > -1) {
+      this.player.attack(this._player2);
+      this._player2.attack(this.player);
+    }
+    return this._player2.lifePoints === -1 ? -1 : 1;
+  }
+}
